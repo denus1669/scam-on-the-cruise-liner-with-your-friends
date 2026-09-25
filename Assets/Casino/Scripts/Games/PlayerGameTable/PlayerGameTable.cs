04db463bd193c09bb39d67776b0ce715b98b8cdb
densuh <76003514+denus1669@users.noreply.github.com>
2026-09-25 23:35:44 +0300

    reworked the folders

---
+++ Assets/Casino/Scripts/Games/PlayerGameTable/PlayerGameTable.cs
@@ -0,0 +1,17 @@
+using Assets.Casino.Games;
+using UnityEngine;
+
+public abstract class PlayerGameTable : GameTable
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
